4ae2c7c361cdd71e8fcde63d3c293b09e15b068d
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-11-21 11:46:27 -0600

    finish js-with-html exercise

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+let userColor = prompt("What is your favorite color?");
+alert("Great " + userColor + " is my favoirte color too.");

9b5c6dffd6fb5fb7bec09a3d806d873858f6b3ea
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2022-12-06 16:57:36 -0600

    testing

--- js/external.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-console.log("Hello from external JavaScript");
-alert('Welcome to my Website!');
-let userColor = prompt("What is your favorite color?");
-let message = "Great " + userColor + " is my favorite color too.";
-alert(message);
